dd40982bd2f11c25ce92dd72850eda3ca0fba821
Mosolov Artem <mosolov.uspeh@gmail.com>
2019-08-11 02:14:00 +0300

    test

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+use vendor\core\Router;
+
+error_reporting(E_ALL);
+
 $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
 define("WWW", __DIR__);
@@ -10,17 +14,15 @@ define("APP", dirname(__DIR__) . "/app");
 require "../vendor/core/Router.php";
 require "../vendor/libs/functions.php";
 
-// require "../app/controllers/Main.php";
-// require "../app/controllers/Posts.php";
-// require "../app/controllers/PostsNew.php";
-
 spl_autoload_register(function ($class) {
+
+    $file = ROOT . '/' . str_replace('\\', '/', $class) . '.php';
     $file = APP . "/controllers/$class.php";
     if (is_file($file)) {
         require_once $file;
     }
+    
 });
-
 // CUSTOME RULES
 Router::add('^pages/?(?P<action>[a-z-]+)?$', ['controller' => 'Posts']);
 
@@ -31,5 +33,3 @@ Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 debug(Router::getRoutes());
 
 Router::dispatch($query);
-
-echo "test";
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace vendor\core;
 
 class Router
 {
